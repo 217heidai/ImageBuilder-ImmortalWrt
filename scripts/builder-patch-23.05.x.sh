893ad75f0cdf72d458952b702e0b0f6f56d4485e
217heidai <217heidai@gmail.com>
2024-05-20 11:41:17 +0800

    切换至 snapshots

--- scripts/builder-patch-23.05.x.sh
+++ scripts/builder-patch-23.05.x.sh
@@ -3,7 +3,7 @@
 echo "Start Builder Patch !"
 echo "Current Path: $PWD"
 
-cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
+cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-x86-64.Linux-x86_64 || exit
 
 # Remove redundant default packages
 sed -i "/luci-app-cpufreq/d" include/target.mk
